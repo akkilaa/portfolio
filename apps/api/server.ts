44301b6d842e2d129b3ade77abea8c7aac26ebbb
akkila <janjic.aleksa911@gmail.com>
2026-05-17 14:47:47 +0200

    Create App layer, to abstract app from server.ts

--- apps/api/server.ts
+++ apps/api/server.ts
@@ -1,11 +1,7 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import app from './src/app.js'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = process.env.PORT ? Number(process.env.PORT) : 3000
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`API listening on port ${port}`)
 })
