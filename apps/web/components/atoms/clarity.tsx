6a92c8fdcd0dad6652367d38a0df9167072a5b68
akkila <janjic.aleksa911@gmail.com>
2026-06-08 13:03:16 +0200

    Remove logs from clarity

--- apps/web/components/atoms/clarity.tsx
+++ apps/web/components/atoms/clarity.tsx
@@ -6,9 +6,7 @@ import { useEffect } from 'react'
 export function ClarityScript() {
   useEffect(() => {
     const id = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID
-    console.log('id', id, process.env.NODE_ENV)
     if (id && process.env.NODE_ENV === 'production') {
-      console.log('INITILAZED')
       clarity.init(id)
     }
   }, [])
